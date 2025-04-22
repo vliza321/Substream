3ca816d491fdfacdd8ce3bda78da4c7019a43e40
PirA <wjw0518@gmail.com>
2025-04-22 15:04:08 +0900

    노드 생성 로직 제작
    노드 생성 로직 제작 완료
    
    UI를 통해 특정 노드만 활성화 하는 로직 구현 남음

---
+++ Assets/Wee/Stage/StageManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public List<List<StageNode>> stages;
+    public int currentLevel;
+    public StageNode currentStage;
+    public int maxLevel;
+
+    public void Initialize()
+    {
+        currentLevel = 0;
+        stages = new List<List<StageNode>>();
+
+        for (int i =0; i < maxLevel; i++)
+        {
+            List<StageNode> stageLevel = new List<StageNode>();
+            int numOfNodes = Random.Range(1, 4);
+            for (int j = 0; j < numOfNodes; j++)
+            {
+                StageNode stageNode = new StageNode();
+                stageNode.Initialize(i,j);
+                stageLevel.Add(stageNode);
+            }
+            stages.Add(stageLevel);
+        }
+
+        for (int i = 0; i < stages.Count; i++)
+        {
+            Debug.Log("Level " + i + " has " + stages[i].Count + " nodes.");
+        }
+
+        for (int i = 0; i < maxLevel - 1; i++)
+        {
+            var nextNodes = stages[i + 1];
+            var currentNodes = stages[i];
+
+            foreach (var nextNode in nextNodes)
+            {
+                int numberOfConnections = Random.Range(1, currentNodes.Count+1);
+                
+                List<int> currentNodeIndex = Enumerable.Range(0, currentNodes.Count).ToList();
+                currentNodeIndex = currentNodeIndex.OrderBy(_ => Random.value).ToList();
+                currentNodeIndex = currentNodeIndex.Take(numberOfConnections).ToList();
+
+                foreach (var index in currentNodeIndex)
+                {
+                    currentNodes[index].nextNodes.Add(nextNode);
+                    numberOfConnections--;
+                }
+            }
+
+            foreach (var node in currentNodes)
+            {
+                if (node.nextNodes.Count == 0)
+                {
+                    int randomIndex = Random.Range(0, nextNodes.Count);
+                    node.nextNodes.Add(nextNodes[randomIndex]);
+                }
+            }
+        }
+
+        for(int i = 0; i < stages.Count; i++)
+        {
+            for (int j = 0; j < stages[i].Count; j++)
+            {
+                for(int k = 0; k < stages[i][j].nextNodes.Count; k++)
+                {
+                    Debug.Log("Node " + j + " in Level " + i + " has " + stages[i][j].nextNodes[k].StageID + " next nodes.");
+                }
+            }
+        }
+    }
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Wee/Stage/StageNode.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageNode : MonoBehaviour
+{
+    public string StageID;
+    public int levelIndex;
+    public List<StageNode> nextNodes;
+    public StageNodeData stageNodeData;
+
+    public bool isCleared;
+    public bool isActive;
+
+    public void Initialize(int inLevelInedex, int IDNum)
+    {
+        levelIndex = inLevelInedex;
+        isCleared = false;
+        isActive = false;
+        nextNodes = new List<StageNode>();
+        StageID = inLevelInedex.ToString() + "_" + IDNum.ToString();
+    }
+
+    public void ClearStage()
+    {
+        isCleared = true;
+        isActive = false;
+
+        for (int i = 0; i < nextNodes.Count; i++)
+        {
+            if (nextNodes[i] != null)
+            {
+                nextNodes[i].SetActive();
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetActive()
+    {
+        isActive = true;
+    }
+}
